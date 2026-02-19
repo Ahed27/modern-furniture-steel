d39fead7b06596e85dcc988699912f56643809a5
Ahed27 <ahedjamous11@gmail.com>
2026-02-19 19:14:18 +0100

    add all
    Signed-off-by: Ahed27 <ahedjamous11@gmail.com>

--- backend/server.js
+++ backend/server.js
@@ -6,3 +6,6 @@ const sequelize = require('./src/config/database');
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log (`Server running on port ${PORT}`));
