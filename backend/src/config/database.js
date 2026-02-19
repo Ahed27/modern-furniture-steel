d39d621e7c1b9189040711b1a40ea2591ce2b9c8
Ahed27 <ahedjamous11@gmail.com>
2026-02-19 19:58:54 +0100

    add database
    Signed-off-by: Ahed27 <ahedjamous11@gmail.com>

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,18 +1,14 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
+require('dotenv').config();
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    console.log("MongoDB connected successfully");
-  } catch (error) {
-    console.error("MongoDB connection failed:", error.message);
-    process.exit(1);
-  }
+    try {
+        await mongoose.connect(process.env.MONGODB_URL);
+        console.log('MongoDB Connected Successfully');
+    } catch (error) {
+        console.error('MongoDB Connection Failed', error);
+        process.exit(1);
+    }
 };
 
 module.exports = connectDB;
